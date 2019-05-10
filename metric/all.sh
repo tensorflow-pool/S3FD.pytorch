90dce154a3de78f6e79186ee70c669bacf09971f
lijc08 <lijc08@vanke.com>
2019-05-11 02:34:48 +0800

    fix

--- metric/all.sh
+++ metric/all.sh
@@ -1,11 +1,14 @@
-python wider_pr_roc.py --dataset=easy --thresh=0.05
-python wider_pr_roc.py --dataset=easy --thresh=0.5
+MODEL_PATH=../model/sfd_face_169000.pth
 
-python wider_pr_roc.py --dataset=medium --thresh=0.05
-python wider_pr_roc.py --dataset=medium --thresh=0.5
+python fddb_pr_roc.py --dataset=easy --thresh=0.05 --model=$MODEL_PATH
+python fddb_pr_roc.py --dataset=easy --thresh=0.5 --model=$MODEL_PATH
 
-python wider_pr_roc.py --dataset=hard --thresh=0.05
-python wider_pr_roc.py --dataset=hard --thresh=0.5
+python wider_pr_roc.py --dataset=easy --thresh=0.05 --model=$MODEL_PATH
+python wider_pr_roc.py --dataset=easy --thresh=0.5 --model=$MODEL_PATH
 
-python fddb_pr_roc.py --dataset=easy --thresh=0.05
-python fddb_pr_roc.py --dataset=easy --thresh=0.5
+python wider_pr_roc.py --dataset=medium --thresh=0.05 --model=$MODEL_PATH
+python wider_pr_roc.py --dataset=medium --thresh=0.5 --model=$MODEL_PATH
+
+python wider_pr_roc.py --dataset=hard --thresh=0.05 --model=$MODEL_PATH
+python wider_pr_roc.py --dataset=hard --thresh=0.5 --model=$MODEL_PATH
+
