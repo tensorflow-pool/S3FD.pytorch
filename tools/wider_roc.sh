0844a37e66e75451af0daa64bdad1632a14bc2d5
lijc08-r730 <lijc08@r730.com>
2019-05-10 23:21:40 +0800

    fix

---
+++ tools/wider_roc.sh
@@ -0,0 +1,8 @@
+python wider_roc.py --dataset=easy --thresh=0.05
+python wider_roc.py --dataset=easy --thresh=0.5
+
+python wider_roc.py --dataset=medium --thresh=0.05
+python wider_roc.py --dataset=medium --thresh=0.5
+
+python wider_roc.py --dataset=hard --thresh=0.05
+python wider_roc.py --dataset=hard --thresh=0.5
