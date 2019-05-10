0973f0d437653a5ffac92105391d82363e4fd691
lijc08 <lijc08@vanke.com>
2019-05-11 02:31:43 +0800

    fix

---
+++ metric/all.sh
@@ -0,0 +1,11 @@
+python wider_pr_roc.py --dataset=easy --thresh=0.05
+python wider_pr_roc.py --dataset=easy --thresh=0.5
+
+python wider_pr_roc.py --dataset=medium --thresh=0.05
+python wider_pr_roc.py --dataset=medium --thresh=0.5
+
+python wider_pr_roc.py --dataset=hard --thresh=0.05
+python wider_pr_roc.py --dataset=hard --thresh=0.5
+
+python fddb_pr_roc.py --dataset=easy --thresh=0.05
+python fddb_pr_roc.py --dataset=easy --thresh=0.5
